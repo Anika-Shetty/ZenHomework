49554a8d4b98b7635c44d3143583688c1e676e33
kittycat1 <38593727+kittycat1@users.noreply.github.com>
2021-04-18 08:41:56 -0700

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Redirect HTTP to HTTPS
+  config.force_ssl = true
+  
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
